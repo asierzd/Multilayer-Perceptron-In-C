d075114454a0deb03d8ab3c1c2d820a375e96358
Asier ZD <asier.zulueta.doalto@gmail.com>
2023-07-16 13:16:27 +0200

    Added some more comments in main.c

--- main.c
+++ main.c
@@ -5,11 +5,12 @@
 #include <unistd.h>
 #include <termios.h>
 
-#define maxCapas 5
-#define maxNeuronas 5
-#define maxElems 5
+#define maxCapas 5 // Maximo numero de capas del perceptron
+#define maxNeuronas 5 // Maximo numero de neuronas por capa
+#define maxElems 5 // Maximo numero de entradas
 int capacont;
 
+// Capa de un perceptron
 struct capa{
 	float matrizPesos[maxElems][maxNeuronas];
 	float umbral[maxNeuronas];
@@ -17,6 +18,8 @@ struct capa{
 	float salida[maxElems];
 	int numNeuronas;
 };
+
+// Perceptron completo
 struct perceptron{
     struct capa capas[maxCapas];
     int numCapas,numEntradas;
@@ -27,7 +30,6 @@ void getch();
 float f(float x);
 void multiplica(struct perceptron *, int);
 void restaVector(struct perceptron *, int);
-float potencia(float x);
 void configuracion(struct perceptron *);
 void entrada(struct perceptron *, int *, int);
 
@@ -65,14 +67,17 @@ int main()
                 entrada(&per, &fin, capacont);
             else if(capacont == (per.numCapas - 1)){ // Si termina sigue con siguiente entrada
                 ultima=1;
-		for(int i = 0; i < per.capas[capacont-1].numNeuronas; i++) // Entrada capa siguiente=Salida capa anterior
-                    per.capas[capacont].entrada[i] = per.capas[capacont-1].salida[i];
+		for(int i = 0; i < per.capas[capacont - 1].numNeuronas; i++) // Entrada capa siguiente=Salida capa anterior
+                    per.capas[capacont].entrada[i] = per.capas[capacont - 1].salida[i];
             }else{
-                for(int i = 0; i < per.capas[capacont-1].numNeuronas; i++) // Entrada capa siguiente=Salida capa anterior
-                    per.capas[capacont].entrada[i] = per.capas[capacont-1].salida[i];
-            } if(fin != EOF){
+                for(int i = 0; i < per.capas[capacont - 1].numNeuronas; i++) // Entrada capa siguiente=Salida capa anterior
+                    per.capas[capacont].entrada[i] = per.capas[capacont - 1].salida[i];
+            } 
+            
+            // Si todavia quedan entradas
+            if(fin != EOF){
 
-                if(primera){
+                if(primera){ // Si es primera entrada
 
                     switch(per.numEntradas){ // Mostrar entrada
 
@@ -86,19 +91,23 @@ int main()
                                 break;
                         case 5: printf("Entrada: (%.2f, %.2f, %.2f, %.2f, %.2f)    ->",per.capas[capacont].entrada[0],per.capas[capacont].entrada[1],per.capas[capacont].entrada[2],per.capas[capacont].entrada[3],per.capas[capacont].entrada[4]);
                                 break;
-                        default: printf("\n\n**Error al mostrar entradas, numEntradas!={ 1 || 2 || 3 || 4 || 5 } . . .\n\n");
+                        default: printf("\n\n**Error al mostrar entradas, numEntradas != { 1 || 2 || 3 || 4 || 5 } . . .\n\n");
                                 break;
 
                     }
                 }
 
+                // Multiplicar matriz de pesos por entradas
                 multiplica(&per, capacont);
 
-                restaVector(&per, capacont); // Resta umbral a la salida anterior
+                // Resta umbral a la salida anterior
+                restaVector(&per, capacont); 
 
-                for(int i = 0; i < per.capas[capacont].numNeuronas; i++) // Aplicacion de funcion F a salida anterior
+                // Aplicacion de funcion F a salida anterior
+                for(int i = 0; i < per.capas[capacont].numNeuronas; i++) 
                     per.capas[capacont].salida[i] = f(per.capas[capacont].salida[i]);
 
+                // Si estamos en ultima entrada
                 if(ultima){
                     switch(per.capas[capacont].numNeuronas){ // Mostrar salida
 
@@ -118,31 +127,34 @@ int main()
                     }
                 }
 
+                // Reiniciar control
                 if(primera){
-                    primera = 0;
+                    primera = 0; // Siguiente capa ya no es primera entrada
                     capacont++;
                 }else if(ultima){
-                    primera=1; // Para leer nueva entrada
-                    ultima=0;
-                    capacont=0; // Comienza de nuevo con la capa 0
+                    primera = 1; // Para leer nueva entrada
+                    ultima = 0; // Ya no es ultima es primera entrada
+                    capacont = 0; // Comienza de nuevo con la capa 0
                 }else 
-                    capacont++;
+                    capacont++; // Siguiente capa
 
-            }else printf("\nEjecucion para todas las entradas terminada con exito.\n");
+            }else // Se acabaron las entradas
+                printf("\nEjecucion para todas las entradas terminada con exito.\n");
 
         }while(capacont < per.numCapas && fin != EOF);
 
 
     }
+
     // Cerrar fichero entradas
     fclose(per.entrada);
 
     return 0;
 }
 
-// Funcion F
+// Funcion F: f(x) = 1 / (1 + e^(-20*x))
 float f(float x){ 
-    double resul=0;
+    double resul = 0;
     x *= -20;
 
     resul = 1 / (1 + pow(M_E, x));
@@ -153,21 +165,22 @@ float f(float x){
 // Lectura de fichero de entrada
 void entrada(struct perceptron *per, int *fin, int capacont){ 
 
+        // Leemos fila completa dependiendo del numero de entradas
         switch(per->numEntradas){
 
-        case 1: *fin=fscanf(per->entrada,"%f",&per->capas[capacont].entrada[0]);
-                break;
-        case 2: *fin=fscanf(per->entrada,"%f %f", &per->capas[capacont].entrada[0], &per->capas[capacont].entrada[1]);
-                break;
-        case 3: *fin=fscanf(per->entrada,"%f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2]);
-                break;
-        case 4: *fin=fscanf(per->entrada,"%f %f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2],&per->capas[capacont].entrada[3]);
-                break;
-        case 5: *fin=fscanf(per->entrada,"%f %f %f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2],&per->capas[capacont].entrada[3],&per->capas[capacont].entrada[4]);
-                break;
-        default: printf("\n**Error: No hay entrada valida al perceptron . . .\n");
-                 exit(1);
-                 break;
+            case 1: *fin=fscanf(per->entrada,"%f",&per->capas[capacont].entrada[0]);
+                    break;
+            case 2: *fin=fscanf(per->entrada,"%f %f", &per->capas[capacont].entrada[0], &per->capas[capacont].entrada[1]);
+                    break;
+            case 3: *fin=fscanf(per->entrada,"%f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2]);
+                    break;
+            case 4: *fin=fscanf(per->entrada,"%f %f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2],&per->capas[capacont].entrada[3]);
+                    break;
+            case 5: *fin=fscanf(per->entrada,"%f %f %f %f %f",&per->capas[capacont].entrada[0],&per->capas[capacont].entrada[1],&per->capas[capacont].entrada[2],&per->capas[capacont].entrada[3],&per->capas[capacont].entrada[4]);
+                    break;
+            default: printf("\n**Error: No hay entrada valida al perceptron . . .\n");
+                    exit(1);
+                    break;
 
         }
 }
@@ -181,56 +194,65 @@ void configuracion(struct perceptron *per){
         printf("\n**Error al abrir el fichero de configuracion del perceptron . . .\n");
     else{ // Fichero correctamente abierto
 
-        fscanf(configuracion, "%d", &per->numCapas); // Obtengo Numero de Capas
+        // Obtengo Numero de Capas
+        fscanf(configuracion, "%d", &per->numCapas); 
         if(per->numCapas > maxCapas){
             printf("\n**Error: Sobrepasa el maximo de capas que permite este perceptron . . .\n");
             exit(1);
         }
 
-        fscanf(configuracion, "%d", &per->numEntradas); // Obtengo Numero de Entradas
+        // Obtengo Numero de Entradas
+        fscanf(configuracion, "%d", &per->numEntradas); 
         if(per->numEntradas > maxElems){
             printf("\n**Error: Sobrepasa el maximo de entradas que permite este perceptron . . .\n");
             exit(1);
         }
 
+        // Configurar cada capa
         for(int i = 0; i < per->numCapas; i++){
             //fflush(stdin);
-            fscanf(configuracion, "%d", &per->capas[i].numNeuronas); // Obtengo Numero de Neuronas
+
+            // Obtengo Numero de Neuronas
+            fscanf(configuracion, "%d", &per->capas[i].numNeuronas); 
             if(per->capas[i].numNeuronas > maxNeuronas){
                 printf("\n**Error: Sobrepasa el maximo de neuronas que permite este perceptron . . .\n");
                 exit(1);
             }
 
+            // Obtengo matriz de pesos
             for(int j = 0; j < per->numEntradas; j++){
                 for(int k = 0; k < per->capas[i].numNeuronas; k++){
                         fscanf(configuracion, "%f ", &per->capas[i].matrizPesos[j][k]);
                 }
             }
 
+            // Para leer valores float de un fichero en formato string usamos
+            // funcion strof para convertir string a float con un puntero char
             char *pEnd; // string->float
             char umbral[10];
             fgets(umbral, 9, configuracion);
 
-            switch(per->capas[i].numNeuronas){ // Para multicapa de hasta 5
-                case 1: per->capas[i].umbral[0] = strtof(umbral,&pEnd);
+            // Para multicapa de hasta 5
+            switch(per->capas[i].numNeuronas){ 
+                case 1: per->capas[i].umbral[0] = strtof(umbral, &pEnd);
                         break;
-                case 2: per->capas[i].umbral[0] = strtof(umbral,&pEnd);
-                        per->capas[i].umbral[1] = strtof(pEnd,NULL);
+                case 2: per->capas[i].umbral[0] = strtof(umbral, &pEnd);
+                        per->capas[i].umbral[1] = strtof(pEnd, NULL);
                         break;
-                case 3: per->capas[i].umbral[0] = strtof(umbral,&pEnd);
-                        per->capas[i].umbral[1] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[2] = strtof(pEnd,NULL);
+                case 3: per->capas[i].umbral[0] = strtof(umbral, &pEnd);
+                        per->capas[i].umbral[1] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[2] = strtof(pEnd, NULL);
                         break;
-                case 4: per->capas[i].umbral[0] = strtof(umbral,&pEnd);
-                        per->capas[i].umbral[1] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[2] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[3] = strtof(pEnd,NULL);
+                case 4: per->capas[i].umbral[0] = strtof(umbral, &pEnd);
+                        per->capas[i].umbral[1] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[2] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[3] = strtof(pEnd, NULL);
                         break;
-                case 5: per->capas[i].umbral[0] = strtof(umbral,&pEnd);
-                        per->capas[i].umbral[1] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[2] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[3] = strtof(pEnd,&pEnd);
-                        per->capas[i].umbral[4] = strtof(pEnd,NULL);
+                case 5: per->capas[i].umbral[0] = strtof(umbral, &pEnd);
+                        per->capas[i].umbral[1] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[2] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[3] = strtof(pEnd, &pEnd);
+                        per->capas[i].umbral[4] = strtof(pEnd, NULL);
                         break;
                 default:printf("\n**Error al leer vector umbral . . .\n");
                         exit(1);
@@ -239,6 +261,7 @@ void configuracion(struct perceptron *per){
 
         }
 
+        // Cerrar fichero configuracion
         fclose(configuracion);
 
         // Carga de la configuracion del perceptron multicapa
@@ -247,26 +270,28 @@ void configuracion(struct perceptron *per){
         printf("Numero de Capas: %d",per->numCapas);
         printf("\nNumero de Entradas: %d",per->numEntradas);
 
-        for(int i=0;i<per->numCapas;i++){
+        // Mostrar configuracion del perceptron multicapa
+        for(int i = 0; i < per->numCapas; i++){
 
             printf("\n\nCargando capa . . .\n");
             //getch();
-            printf("\n Numero de entradas: %d",per->numEntradas);
-            printf("\n Numero de neuronas: %d\n",per->capas[i].numNeuronas);
+            printf("\n Numero de entradas: %d", per->numEntradas);
+            printf("\n Numero de neuronas: %d\n", per->capas[i].numNeuronas);
 
-            for(int j=0;j<per->numEntradas;j++)// Matriz de Pesos
-                for(int k=0;k<per->capas[i].numNeuronas;k++)
-                    printf("w[%d][%d]=%f\n",j,k,per->capas[i].matrizPesos[j][k]);
+            for(int j = 0; j < per->numEntradas; j++)// Matriz de Pesos
+                for(int k = 0; k < per->capas[i].numNeuronas; k++)
+                    printf("w[%d][%d]=%f\n", j, k, per->capas[i].matrizPesos[j][k]);
 
             printf("\nUmbrales\n");
 
-            for(int j=0;j<per->capas[i].numNeuronas;j++)
+            for(int j = 0; j < per->capas[i].numNeuronas; j++)
 
-                printf("umbral[%d]=%f\n",j,per->capas[i].umbral[j]);
+                printf("umbral[%d]=%f\n", j, per->capas[i].umbral[j]);
         }
     }
 }
 
+// Funcion para multiplicar matriz de pesos por las entradas
 void multiplica(struct perceptron *per, int capacont){
 	float suma = 0;
 	for(int i = 0; i < per->capas[capacont].numNeuronas; i++){
@@ -278,6 +303,7 @@ void multiplica(struct perceptron *per, int capacont){
 	}
 }
 
+// Funcion para restar al vector de salida el vector umbral
 void restaVector(struct perceptron *per, int capacont){
 
     for(int i = 0; i < per->capas[capacont].numNeuronas; i++){
@@ -286,7 +312,7 @@ void restaVector(struct perceptron *per, int capacont){
 
 }
 
-// Used to pause execution until pressed Enter button
+// Used to pause execution until pressed Enter button, only for debugging
 void getch(){
     char buf=0;
     struct termios old={0};
